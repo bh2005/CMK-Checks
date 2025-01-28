c6cef180071d590fb3976ebcdf3bf9729ccddc22
Bernd Holzhauer <bernd.holzhauer@k-plus-s.com>
2025-01-28 08:28:54 +0100

    adding new check

--- extremecloudiq/draft-agent/usr/lib/web/plugins/wato/xiq_agent.py
+++ extremecloudiq/draft-agent/usr/lib/web/plugins/wato/xiq_agent.py
@@ -2,7 +2,6 @@
 
 # XIQ Extreme Cloud settings
 
-
 from cmk.gui.plugins.wato import (
     HostRulespec,
     rulespec_registry,
@@ -11,16 +10,56 @@ from cmk.gui.valuespec import (
     Dictionary,
     TextAscii,
     Password,
+    Integer,
+    CascadingDropdown,
 )
 
 def _valuespec_example_rule():
     return Dictionary(
+        title=_("Benutzername und Passwort"),
+        help=_("Konfiguration von Benutzername, Passwort, API-URL und Port"),
         elements=[
             ("username", TextAscii(title=_("Benutzername"), help=_("Geben Sie den Benutzernamen ein"))),
             ("password", Password(title=_("Passwort"), help=_("Geben Sie das Passwort ein"))),
+            ("api_settings", CascadingDropdown(
+                title=_("API-Einstellungen"),
+                choices=[
+                    ("default", _("Standard-API-Einstellungen"), Dictionary(
+                        elements=[
+                            ("url", TextAscii(
+                                title=_("API URL"),
+                                help=_("Die URL der API (Standard: https://api.extremecloudiq.com)"),
+                                default_value="https://api.extremecloudiq.com",
+                            )),
+                            ("port", Integer(
+                                title=_("Port"),
+                                help=_("Der Port für die API (Standard: 443)"),
+                                default_value=443,
+                                minvalue=1,
+                                maxvalue=65535,
+                            )),
+                        ],
+                    )),
+                    ("custom", _("Benutzerdefinierte Einstellungen"), Dictionary(
+                        elements=[
+                            ("url", TextAscii(
+                                title=_("API URL"),
+                                help=_("Die URL der API"),
+                                allow_empty=False,
+                            )),
+                            ("port", Integer(
+                                title=_("Port"),
+                                help=_("Der Port für die API"),
+                                default_value=443,
+                                minvalue=1,
+                                maxvalue=65535,
+                            )),
+                        ],
+                    )),
+                ],
+                default_value="default",
+            )),
         ],
-        title=_("Benutzername und Passwort"),
-        help=_("Konfiguration von Benutzername und Passwort"),
     )
 
 rulespec_registry.register(
@@ -30,6 +69,3 @@ rulespec_registry.register(
         valuespec=_valuespec_example_rule,
     )
 )
-
-
-### test comment
